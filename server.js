2030213f914c77bdbcc47febc2b1163b46fe524f
Julien Quartier <julien.quartier@bluewin.ch>
2019-11-18 21:59:39 +0100

    Improving safety

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const fs = require("fs");
 const fetch = require("node-fetch");
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(Conf.api_key, {polling: true});
+const bot = new TelegramBot(Conf.api_key, { polling: true });
 
 const regex = /<li>Score.*<span>(\d+)<\/span>/;
+
+let loading = false;
 
 bot.onText(/\/git/, async (msg, match) => {
     const chatId = msg.chat.id;
@@ -20,12 +22,13 @@ bot.onText(/\/rootme/, async (msg, match) => {
 
     const leaderboard = await getRootmeBoard();
 
+    // Format the result table
     const resp = "<b>Score:</b> \r\n" + leaderboard.reduce((acc, user, i) => {
-        const spaces = i < 9 ? "\t\t": "";
+        const spaces = i < 9 ? "\t\t" : "";
         return acc + `\t\t\t${spaces}${i + 1}:\t\t\t<code>${user.username}</code>  <b>${user.score}</b> \r\n`;
     }, "");
 
-    bot.sendMessage(chatId, resp,{parse_mode : "HTML"});
+    bot.sendMessage(chatId, resp, { parse_mode: "HTML" });
 });
 
 bot.onText(/\/add_rootme (.+)/, async (msg, match) => {
@@ -36,9 +39,9 @@ bot.onText(/\/add_rootme (.+)/, async (msg, match) => {
 
     const result = await fetch(`https://www.root-me.org/${username}?lang=fr`);
 
-    if(result.status === 200){
+    if (result.status === 200) {
 
-        if(!usernames.includes(username)) {
+        if (!usernames.includes(username)) {
             // invalidate buffer
             writeFile("./rootme-buffer.json", {
                 timestamp: 0,
@@ -61,15 +64,21 @@ bot.onText(/\/add_rootme (.+)/, async (msg, match) => {
     bot.sendMessage(chatId, resp);
 });
 
-async function getRootmeBoard(){
+/**
+ * Get all user's scores
+ * @returns {Promise<this|*>}
+ */
+async function getRootmeBoard() {
 
     const usernames = await readConfig("rootme");
 
-    try{
+    await waitLoadingFinished();
+
+    try {
         const bufferFile = await readFile("./rootme-buffer.json");
 
         // buffer available
-        if(bufferFile != null && bufferFile.timestamp + 60 * 10 * 1000 > Date.now()){
+        if (bufferFile != null && bufferFile.timestamp + 60 * 10 * 1000 > Date.now()) {
             return bufferFile.data;
         }
     } catch (e) {
@@ -77,9 +86,11 @@ async function getRootmeBoard(){
     }
 
 
+    loading = true;
+
     let leaderboard = [];
 
-    for(let username of usernames){
+    for (let username of usernames) {
         try {
             leaderboard.push(await getRootMeScore(username));
         } catch (e) {
@@ -94,32 +105,61 @@ async function getRootmeBoard(){
         data: leaderboard
     });
 
+    loading = false;
+
     return leaderboard;
 }
 
+/**
+ * Get score for a single user
+ * @param username
+ * @returns {Promise<{score: number, username: *}|{score: *, username: *}>}
+ */
 async function getRootMeScore(username) {
-    const response = await fetch(`https://www.root-me.org/${username}?lang=fr`);
+    let response = await fetch(`https://www.root-me.org/${username}?lang=fr`);
 
-    return new Promise((async (resolve) => {
-        if(response.status === 429){
+    // If request rejected for too many requests. Retry after in little time out
+    while (response.status === 429) {
+        console.error("Request rejected (429), wait a little bit before retrying")
+        response = await new Promise(resolve => {
             setTimeout(async () => {
-                resolve(await getRootMeScore(username));
-            }, 500);
+                resolve(await fetch(`https://www.root-me.org/${username}?lang=fr`));
+            }, 1500)
+        });
+    }
+
+    // If success return the score
+    if (response.status === 200) {
+        const html = await response.text();
+        const match = html.match(regex);
+
+        // If a score is found return it, else 0
+        if (match != null) {
+            const score = match[1];
+            console.log("resolve", { score, username })
+            return { score, username };
         } else {
-            const html = await response.text();
-            const match = html.match(regex);
-
-            if(match != null){
-                const score = match[1];
-                console.log("resolve", {score,username})
-                resolve({score,username});
-            } else {
-                resolve({score: 0, username});
-            }
+            return { score: 0, username };
         }
-    }));
+    } else {
+        console.error("Error:", response.status)
+    }
+}
 
+/**
+ * Wait until loading is true
+ * @returns {Promise<void>}
+ */
+async function waitLoadingFinished() {
+    if (loading)
+        console.log("Wait for buffer construction");
 
+    // Wait until loading is finished
+    while (loading) {
+        await new Promise(resolve => {
+            setTimeout(() => resolve(), 1000)
+        });
+    }
 }
 
 function readConfig(name) {
@@ -133,8 +173,8 @@ function writeConfig(name, data) {
 
 function readFile(file) {
     return new Promise((resolve, reject) => {
-        fs.readFile(file, function(err, buf) {
-            if(err)
+        fs.readFile(file, function (err, buf) {
+            if (err)
                 reject(err);
             else
                 resolve(JSON.parse(buf));
@@ -144,8 +184,8 @@ function readFile(file) {
 
 function writeFile(file, data) {
     return new Promise((resolve, reject) => {
-        fs.writeFile(file, JSON.stringify(data) , function(err) {
-            if(err)
+        fs.writeFile(file, JSON.stringify(data), function (err) {
+            if (err)
                 reject(err);
             else
                 resolve();
