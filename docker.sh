0e9c4a5232f8ce4a511651a3444b19804232e041
julien <julien.quartier@heig-vd.ch>
2019-11-20 14:01:09 +0100

    Docker detach

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 docker build -t olympia .
 
-docker run -ti -v /root/olympia-telegram-boot/dynamic_config:/tmp/dynamic_config olympia
+docker run -d -v /root/olympia-telegram-boot/dynamic_config:/tmp/dynamic_config olympia
